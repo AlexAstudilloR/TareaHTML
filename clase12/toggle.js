3b264b6280527eefef4645c9054936e3717c32f3
AlexAstudillo <aastudillor3@unemi.edu.ec>
2023-05-11 18:04:46 -0500

    Modo oscuro implementado

---
+++ clase12/toggle.js
@@ -0,0 +1,11 @@
+let toogle= document.getElementById('container');
+let body= document.querySelector('body');
+let header= document.querySelector('header');
+let footer= document.querySelector('footer');
+
+toogle.onclick= function(){
+  toogle.classList.toggle('active');
+  body.classList.toggle('active');
+  header.classList.toggle('active');
+  footer.classList.toggle('active');
+}
