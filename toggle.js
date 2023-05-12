4e7a12b0b944dff57f3e9ac2b4328ee1b7770be2
AlexAstudillo <aastudillor3@unemi.edu.ec>
2023-05-12 07:55:57 -0500

    Version Final

--- toggle.js
+++ toggle.js
@@ -2,10 +2,21 @@ let toogle= document.getElementById('container');
 let body= document.querySelector('body');
 let header= document.querySelector('header');
 let footer= document.querySelector('footer');
-
+let dropdownmenu= document.getElementById('lista1');
+let dropdownmenu2= document.getElementById('lista2');
+let dropdownmenu3= document.getElementById('lista3');
+let dropdownmenu4= document.getElementById('lista4');
+let dropdownmenu5= document.getElementById('lista5');
+let dropdownmenu6= document.getElementById('lista6');
 toogle.onclick= function(){
   toogle.classList.toggle('active');
   body.classList.toggle('active');
   header.classList.toggle('active');
   footer.classList.toggle('active');
+  dropdownmenu.classList.toggle('active');
+  dropdownmenu3.classList.toggle('active');
+  dropdownmenu4.classList.toggle('active');
+  dropdownmenu2.classList.toggle('active');
+  dropdownmenu5.classList.toggle('active');
+  dropdownmenu6.classList.toggle('active');
 }
